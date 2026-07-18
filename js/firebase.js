3ca76a6134f42111438cd526d9033428d8450d30
Daniel <daniel.moser@sjsu.edu>
2026-07-18 22:53:08 +0000

    Added initial database

--- js/firebase.js
+++ js/firebase.js
@@ -2,6 +2,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 import {getAuth,GoogleAuthProvider,signInWithPopup,signOut,onAuthStateChanged} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -25,5 +26,7 @@ const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+const db = getFirestore(app);
 
-export {auth,googleProvider,signInWithPopup,signOut,onAuthStateChanged};
+
+export { auth, db, googleProvider, signInWithPopup, signOut, onAuthStateChanged};
